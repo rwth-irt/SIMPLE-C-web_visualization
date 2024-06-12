aeed61aceacbbe9da734b54456bc2ad765ce77eb
Max Viehl <max.viehl@posteo.de>
2024-06-12 17:36:08 +0200

    Add basic pointcloud, clicking does not work anymore

--- src/main.ts
+++ src/main.ts
@@ -18,18 +18,42 @@ controls.addEventListener('change', function () {
 });
 
 
-// world
-const geometry = new THREE.ConeGeometry(10, 30, 4, 1);
-const material = new THREE.MeshPhongMaterial({ color: 0xffffff, flatShading: true });
-for (let i = 0; i < 500; i++) {
-    const mesh = new THREE.Mesh(geometry, material);
-    mesh.position.x = Math.random() * 1600 - 800;
-    mesh.position.y = 0;
-    mesh.position.z = Math.random() * 1600 - 800;
-    mesh.updateMatrix();
-    mesh.matrixAutoUpdate = false;
-    scene.add(mesh);
+// add point cloud
+function generatePointCloudGeometry(color: THREE.Color, width: number, length: number, scale: number) {
+    const geometry = new THREE.BufferGeometry();
+    const numPoints = width * length;
+    const positions = new Float32Array(numPoints * 3);
+    const colors = new Float32Array(numPoints * 3);
+    let k = 0;
+    for (let i = 0; i < width; i++) {
+        for (let j = 0; j < length; j++) {
+            const u = i / width;
+            const v = j / length;
+            const x = u - 0.5;
+            const y = (Math.cos(u * Math.PI * 4) + Math.sin(v * Math.PI * 8)) / 20;
+            const z = v - 0.5;
+
+            positions[3 * k] = x * scale;
+            positions[3 * k + 1] = y * scale;
+            positions[3 * k + 2] = z * scale;
+
+            const intensity = (y + 0.1) * 5;
+            colors[3 * k] = color.r * intensity;
+            colors[3 * k + 1] = color.g * intensity;
+            colors[3 * k + 2] = color.b * intensity;
+
+            k++;
+        }
+    }
+    geometry.setAttribute('position', new THREE.BufferAttribute(positions, 3));
+    geometry.setAttribute('color', new THREE.BufferAttribute(colors, 3));
+    geometry.computeBoundingBox();
+    return geometry;
 }
+const geometry = generatePointCloudGeometry(new THREE.Color(1, 0, 0), 10, 10, 10);
+const material = new THREE.PointsMaterial({ size: 0.1, vertexColors: true });
+const points = new THREE.Points(geometry, material);
+scene.add(points);
 
 //controls.update() must be called after any manual changes to the camera's transform
 camera.position.set(0, 10, 0);
