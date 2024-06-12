8a4eb0fb4a372d0cd22df587bca6a017d681de62
Max Viehl <max.viehl@posteo.de>
2024-06-12 17:16:06 +0200

    Add three.js template

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,36 @@
-import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.ts'
+import * as THREE from 'three';
+import { ArcballControls } from 'three/addons/controls/ArcballControls.js';
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`
+const renderer = new THREE.WebGLRenderer({
+    antialias: true
+});
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+const scene = new THREE.Scene();
+scene.background = new THREE.Color(0xcccccc);
+
+
+const camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 1, 10000);
+const controls = new ArcballControls(camera, renderer.domElement, scene);
+controls.addEventListener('change', function () {
+    renderer.render(scene, camera);
+});
+
+
+// world
+const geometry = new THREE.ConeGeometry(10, 30, 4, 1);
+const material = new THREE.MeshPhongMaterial({ color: 0xffffff, flatShading: true });
+for (let i = 0; i < 500; i++) {
+    const mesh = new THREE.Mesh(geometry, material);
+    mesh.position.x = Math.random() * 1600 - 800;
+    mesh.position.y = 0;
+    mesh.position.z = Math.random() * 1600 - 800;
+    mesh.updateMatrix();
+    mesh.matrixAutoUpdate = false;
+    scene.add(mesh);
+}
+
+//controls.update() must be called after any manual changes to the camera's transform
+camera.position.set(0, 10, 0);
+controls.update();
