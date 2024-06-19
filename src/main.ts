2b590b9491b1ed336611e9c85df334eeb76f9260
Max Viehl <max.viehl@posteo.de>
2024-06-19 13:21:44 +0200

    Add connect button and test messages

--- src/main.ts
+++ src/main.ts
@@ -5,9 +5,12 @@ import { set_callback } from './ws';
 // Basic Three.js setup
 const scene = new THREE.Scene();
 scene.background = new THREE.Color(0xcccccc);
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+let width = 800;
+let height = 600;
+const camera = new THREE.PerspectiveCamera(75, width / height, 0.1, 1000);
+camera.position.set(0, 0, 10);
 const renderer = new THREE.WebGLRenderer();
-renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.setSize(width, height);
 document.body.appendChild(renderer.domElement);
 
 let current_group: THREE.Group | undefined = undefined;
@@ -24,12 +27,20 @@ set_callback(g => new_objects(g));
 
 // OrbitControls for camera
 const controls = new OrbitControls(camera, renderer.domElement);
-camera.position.set(0, 0, 10);
 controls.update();
 
 // Raycaster and mouse vector
 const raycaster = new THREE.Raycaster();
 const mouse = new THREE.Vector2();
+
+// TODO test object: floor
+
+const geometry = new THREE.PlaneGeometry(20, 20);
+const material = new THREE.MeshBasicMaterial({ color: 0xaaaaaa });
+const plane = new THREE.Mesh(geometry, material);
+scene.add(plane);
+console.log(plane.position)
+console.log(new THREE.Euler().setFromQuaternion(plane.quaternion))
 
 // Event listener for click
 renderer.domElement.addEventListener('dblclick', (event) => {
--- src/ws.ts
+++ src/ws.ts
@@ -12,10 +12,8 @@ interface CalibMessage {
         from: string,
         to: string,
         trafo: {
-            R: number[][]
             t: number[]
             R_quat: number[]
-            R_sensitivity: number[][]
         }
     }[]
 }
@@ -29,7 +27,6 @@ function connect() {
 
     let socket = new WebSocket("ws://localhost:6789");
     socket.addEventListener("message", (event) => {
-        console.log("Got message");
         let data: CalibMessage = JSON.parse(event.data);
         new_calib_message(data);
     });
@@ -39,14 +36,12 @@ function connect() {
     });
     socket.addEventListener("error", () => {
         connected = false;
-        setTimeout(connect, 100);
     });
     socket.addEventListener("close", () => {
         connected = false;
-        setTimeout(connect, 100);
     });
 }
-connect();
+document.getElementById("connect_btn")!.addEventListener("click", connect);
 
 
 let colors = {
@@ -80,7 +75,7 @@ function new_calib_message(msg: CalibMessage) {
             colors[3 * i + 1] = c[1];
             colors[3 * i + 2] = c[2];
         }
-        let geometry = new THREE.BufferGeometry();
+        let geometry = new THREE.BufferGeometry(); // TODO use cached variant!!!!
         geometry.setAttribute('position', new THREE.BufferAttribute(vertices, 3));
         geometry.setAttribute('color', new THREE.BufferAttribute(colors, 3));
         let material = new THREE.PointsMaterial({ size: 0.3, vertexColors: true });
@@ -112,3 +107,94 @@ function get_color_from_index(i: number, marker_index?: number): number[] {
         return colors.cluster;
     return colors.any;
 }
+
+
+// Debugging: Schedule some messages
+function schedule_test_msgs() {
+    let test_message1: CalibMessage = {
+        frames: [
+            {
+                topic: "a",
+                points: [1, 2, 3, 1, 2, 4, 2, 3, 5, 4, 3, 5, 3, 2, 4, 4, 4, 4, 3, 2, 3, -1, -1, -1],
+                colors: [-2, -1, 0, 1, -2, -1, 0, 1,],
+                marker_index: 3
+            }
+        ],
+        transformations: []
+    }
+
+    let test_message2: CalibMessage = {
+        frames: [
+            {
+                topic: "a",
+                points: [1.3, 2, 3.3, 1.3, 2.3, 4, 2.3, 3, 5.3, 4.3, 3, 6, 2, 2.3, 4, 5, 4.3, 4, 3, -2, -3, -1.3, 1, -1.3],
+                colors: [-2, -1, 0, 1, -2, -1, 0, 1,],
+                marker_index: 3 // TODO is not blue, implement!
+            }
+        ],
+        transformations: []
+    }
+
+    let test_message3: CalibMessage = {
+        frames: [
+            {
+                topic: "a",
+                points: [1.3, 2, 3.3, 1.3, 2.3, 4, 2.3, 3, 5.3, 4.3, 3, 6, 2, 2.3, 4, 5, 4.3, 4, 3, -2, -3, -1.3, 1, -1.3],
+                colors: [-1, -1, -1, -1, -1, -1, -1, -1],
+                marker_index: 3
+            },
+            {
+                topic: "b",
+                points: [1, 2, 3.3, 1.3, 2, 4, 2.3, 3, 5, 4.3, 3, 6, 2, 2.3, 4, 5, 4, 4, 3, -2, -3, -1, 1, -1.2],
+                colors: [1, 1, 1, 1, 1, 1, 1, 1],
+            },
+            {
+                topic: "c",
+                points: [1, 2, 3.3, 1.3, 2, 4, 2.3, 3, 5, 4.3, 3, 6, 2, 2.3, 4, 5, 4, 4, 3, 2, 3, 1, 1, 1.2],
+                colors: [1, 1, 1, 1, 1, 1, 1, 1],
+            }
+        ],
+        transformations: [
+            {
+                from: "a",
+                to: "b",
+                trafo: {
+                    t: [0, 0, 0],
+                    R_quat: [1, 0, 0, 0]
+                }
+            }
+        ]
+    }
+
+    let test_message4: CalibMessage = {
+        frames: [
+            {
+                topic: "a",
+                points: [1.3, 2, 3.3, 1.3, 2.3, 4, 2.3, 3, 5.3, 4.3, 3, 6, 2, 2.3, 4, 5, 4.3, 4, 3, -2, -3, -1.3, 1, -1.3],
+                colors: [-1, -1, -1, -1, -1, -1, -1, -1],
+                marker_index: 3
+            },
+            {
+                topic: "b",
+                points: [1, 2, 3.3, 1.3, 2, 4, 2.3, 3, 5, 4.3, 3, 6, 2, 2.3, 4, 5, 4, 4, 3, -2, -3, -1, 1, -1.2],
+                colors: [1, 1, 1, 1, 1, 1, 1, 1],
+                marker_index: 3
+            }
+        ],
+        transformations: [
+            {
+                from: "a",
+                to: "b",
+                trafo: {
+                    t: [0, 0, 0],
+                    R_quat: [1, 0, 0, 0]
+                }
+            }
+        ]
+    }
+    setTimeout(() => { new_calib_message(test_message1); }, 1000);
+    setTimeout(() => { new_calib_message(test_message2); }, 3000);
+    setTimeout(() => { new_calib_message(test_message3); }, 5000);
+    setTimeout(() => { new_calib_message(test_message4); }, 7000);
+}
+schedule_test_msgs();
