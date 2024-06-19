0035024dd568c2ab8624b1baf04a09d4e4934af0
Max Viehl <max.viehl@posteo.de>
2024-06-19 14:53:24 +0200

    Move test function away

--- src/ws.ts
+++ src/ws.ts
@@ -1,6 +1,6 @@
 import * as THREE from 'three';
 
-interface CalibMessage {
+export interface CalibMessage {
     frames: {
         topic: string
         points: number[]
@@ -119,94 +119,3 @@ function get_color_from_index(i: number, frame_index: number, marker_index?: num
         return colors.cluster;
     return basic_colors[frame_index % basic_colors.length];
 }
-
-
-// Debugging: Schedule some messages
-function schedule_test_msgs() {
-    let test_message1: CalibMessage = {
-        frames: [
-            {
-                topic: "a",
-                points: [1, 2, 3, 1, 2, 4, 2, 3, 5, 4, 3, 5, 3, 2, 4, 4, 4, 4, 3, 2, 3, -1, -1, -1],
-                colors: [-2, -1, 0, 1, -2, -1, 0, 1,],
-                marker_index: 3
-            }
-        ],
-        transformations: []
-    }
-
-    let test_message2: CalibMessage = {
-        frames: [
-            {
-                topic: "a",
-                points: [1.3, 2, 3.3, 1.3, 2.3, 4, 2.3, 3, 5.3, 4.3, 3, 6, 2, 2.3, 4, 5, 4.3, 4, 3, -2, -3, -1.3, 1, -1.3],
-                colors: [-2, -1, 0, 1, -2, -1, 0, 1,],
-                marker_index: 3 // TODO is not blue, implement!
-            }
-        ],
-        transformations: []
-    }
-
-    let test_message3: CalibMessage = {
-        frames: [
-            {
-                topic: "a",
-                points: [1.3, 2, 3.3, 1.3, 2.3, 4, 2.3, 3, 5.3, 4.3, 3, 6, 2, 2.3, 4, 5, 4.3, 4, 3, -2, -3, -1.3, 1, -1.3],
-                colors: [-1, -1, -1, -1, -1, -1, -1, -1],
-                marker_index: 3
-            },
-            {
-                topic: "b",
-                points: [1, 2, 3.3, 1.3, 2, 4, 2.3, 3, 5, 4.3, 3, 6, 2, 2.3, 4, 5, 4, 4, 3, -2, -3, -1, 1, -1.2],
-                colors: [1, 1, 1, 1, 1, 1, 1, 1],
-            },
-            {
-                topic: "c",
-                points: [1, 2, 3.3, 1.3, 2, 4, 2.3, 3, 5, 4.3, 3, 6, 2, 2.3, 4, 5, 4, 4, 3, 2, 3, 1, 1, 1.2],
-                colors: [1, 1, 1, 1, 1, 1, 1, 1],
-            }
-        ],
-        transformations: [
-            {
-                from: "a",
-                to: "b",
-                trafo: {
-                    t: [0, 0, 0],
-                    R_quat: [1, 0, 0, 0]
-                }
-            }
-        ]
-    }
-
-    let test_message4: CalibMessage = {
-        frames: [
-            {
-                topic: "a",
-                points: [1.3, 2, 3.3, 1.3, 2.3, 4, 2.3, 3, 5.3, 4.3, 3, 6, 2, 2.3, 4, 5, 4.3, 4, 3, -2, -3, -1.3, 1, -1.3],
-                colors: [-1, -1, -1, -1, -1, -1, -1, -1],
-                marker_index: 3
-            },
-            {
-                topic: "b",
-                points: [1, 2, 3.3, 1.3, 2, 4, 2.3, 3, 5, 4.3, 3, 6, 2, 2.3, 4, 5, 4, 4, 3, -2, -3, -1, 1, -1.2],
-                colors: [1, 1, 2, 1, 1, 1, 1, 1],
-                marker_index: 2
-            }
-        ],
-        transformations: [
-            {
-                from: "a",
-                to: "b",
-                trafo: {
-                    t: [0, 0, 0],
-                    R_quat: [1, 0, 0, 0]
-                }
-            }
-        ]
-    }
-    setTimeout(() => { new_calib_message(test_message1); }, 1000);
-    setTimeout(() => { new_calib_message(test_message2); }, 3000);
-    setTimeout(() => { new_calib_message(test_message3); }, 5000);
-    setTimeout(() => { new_calib_message(test_message4); }, 7000);
-}
-// schedule_test_msgs();
