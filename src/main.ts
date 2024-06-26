3b74b562149f22316e749143405e70ce8cd38886
Max Viehl <max.viehl@posteo.de>
2024-06-26 13:02:04 +0200

    Set better default camera position, clean up main.ts

--- src/main.ts
+++ src/main.ts
@@ -4,57 +4,62 @@ import { connect } from './ws';
 import { Sensor } from './sensor_data';
 
 // Basic Three.js setup
-THREE.Object3D.DEFAULT_UP = new THREE.Vector3(0,0,1); // set z up, as in lidar data
+THREE.Object3D.DEFAULT_UP = new THREE.Vector3(0, 0, 1); // set z up, as in lidar data
 const scene = new THREE.Scene();
 scene.background = new THREE.Color(0xfafafa);
 let height = window.innerHeight * 0.8;
 let width = height * 1.3;
 const camera = new THREE.PerspectiveCamera(75, width / height, 0.1, 1000);
-camera.position.set(0, 0, 10);
+camera.position.set(0, 0, 20);
+
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(width, height);
 document.getElementById("threedif")!.appendChild(renderer.domElement);
 
 // OrbitControls for camera
 const controls = new OrbitControls(camera, renderer.domElement);
+controls.target.set(20, 0, 0);
 controls.update();
 
 // Create a marker at the controls' target point
 const marker = new THREE.Mesh(
-    new THREE.SphereGeometry(0.1, 32, 32), 
+    new THREE.SphereGeometry(0.1, 32, 32),
     new THREE.MeshBasicMaterial({ color: 0x000000 })
 );
 scene.add(marker);
 
 // add ws handler
-document.getElementById("connect_btn")!.addEventListener("click", ()=> {
-    let sensors: Map<string, Sensor> = new Map();
-    connect(data => {
-        // this is called on every Websocket message
-        let type: "pointcloud" | "metadata" = data.type;
-        let topic: string = data.topic;
-        if (!sensors.has(topic)) {
-            sensors.set(topic, new Sensor(topic, scene));
-        }
-        let sensor = sensors.get(topic)!;
-        if (type == "metadata") {
-            let payload = data.data as {
-                reflector_locations: number[],
-                transformation: {
-                    t: number[],
-                    R_quat: number[]
-                }
+let sensors: Map<string, Sensor>;
+function on_message(data: any) {
+    // this is called on every Websocket message
+    let type: "pointcloud" | "metadata" = data.type;
+    let topic: string = data.topic;
+    if (!sensors.has(topic)) {
+        sensors.set(topic, new Sensor(topic, scene));
+    }
+    let sensor = sensors.get(topic)!;
+    if (type == "metadata") {
+        let payload = data.data as {
+            reflector_locations: number[],
+            transformation: {
+                t: number[],
+                R_quat: number[]
             }
-            sensor.on_metadata(payload.reflector_locations, payload.transformation);
         }
-        if (type == "pointcloud") {
-            let payload = data.data as {
-                points: number[],
-                colors: number[]
-            }
-            sensor.on_frame(payload.points, payload.colors);
+        sensor.on_metadata(payload.reflector_locations, payload.transformation);
+    }
+    if (type == "pointcloud") {
+        let payload = data.data as {
+            points: number[],
+            colors: number[]
         }
-    });
+        sensor.on_frame(payload.points, payload.colors);
+    }
+}
+
+document.getElementById("connect_btn")!.addEventListener("click", () => {
+    sensors = new Map();
+    connect(data => on_message(data));
 });
 
 
